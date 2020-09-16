42c5014eacbc4ca0354d8700c781c8c5c81426a4
lesleymaraina <lesley.maraina@gmail.com>
2020-09-16 15:42:52 -0400

    Update OAuth.py

--- OAuth.py
+++ OAuth.py
@@ -1,7 +1,7 @@
 from flask import Flask, redirect, url_for
 from flask_dance.contrib.google import make_google_blueprint, google
 
-google_blueprint =  make_google_blueprint(client_id='198432854470-8or4s0j1r899khmq0p72fmi4mmj968iq.apps.googleusercontent.com', client_secret='xSKeuuHrbyQKFTg94ADaJH9B')
+google_blueprint =  make_google_blueprint(client_id=' ', client_secret=' ')
 app.register_blueprint(google_blueprint, url_prefix='/google_login')
 
 @app.route('/google')
